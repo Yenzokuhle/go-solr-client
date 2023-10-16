b70c16ed954a38eacf2b3bbc57c4de05219fa155
Yenzokuhle <yenzom@icloud.com>
2023-10-16 07:55:50 +0200

    Created more helpers

---
+++ src/styles/breakpoints.ts
@@ -0,0 +1,13 @@
+export const breakpoints = {
+  mobileXS: 312,
+  mobileSM: 375,
+  mobileMD: 414,
+  mobileLG: 512,
+  tabletSM: 768,
+  tabletMD: 938,
+  tabletLG: 1024,
+  desktopXS: 1200,
+  desktopMD: 1400,
+  desktopLG: 1600,
+  desktopXL: 1920,
+};
