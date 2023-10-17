2aa25d06ab54333cb70db3dce51ecffbfe37737d
Yenzokuhle <yenzom@icloud.com>
2023-10-17 23:46:42 +0200

    Added new test

--- src/helpers/utils.test.ts
+++ src/helpers/utils.test.ts
@@ -1,5 +1,12 @@
 import { getTotalTopUps } from './utils';
 
 describe('Return the sum of top ups', () => {
-  test('Return R 2000', () => {});
+  test('Return R 300', () => {
+    expect(
+      getTotalTopUps([
+        { name: 'itemOne', value: 200 },
+        { name: 'itemTwo', value: 100 },
+      ])
+    ).toBe(300);
+  });
 });
